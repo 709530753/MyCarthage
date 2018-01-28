4beb507d9e058fabaa406673d0c3b2082621ff61
myxc <70953073@qq.com>
2018-01-28 20:10:52 +0800

    carthage commit

---
+++ MyCarthage/MyCarthageFramework/MyCarthage.swift
@@ -0,0 +1,17 @@
+//
+//  MyCarthage.swift
+//  MyCarthageFramework
+//
+//  Created by myxc on 28/01/2018.
+//  Copyright © 2018 myxc. All rights reserved.
+//
+
+import UIKit
+
+public class MyCarthage: NSObject {
+
+    public static func test() {
+        print("test")
+    }
+    
+}
---
+++ MyCarthage/MyCarthageFramework/MyCarthageFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyCarthageFramework.h
+//  MyCarthageFramework
+//
+//  Created by myxc on 28/01/2018.
+//  Copyright © 2018 myxc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyCarthageFramework.
+FOUNDATION_EXPORT double MyCarthageFrameworkVersionNumber;
+
+//! Project version string for MyCarthageFramework.
+FOUNDATION_EXPORT const unsigned char MyCarthageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCarthageFramework/PublicHeader.h>
+
+
